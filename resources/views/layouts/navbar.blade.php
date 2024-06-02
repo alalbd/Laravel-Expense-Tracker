20886ad530a3c09f225cdeb7dd424b90522fbf20
alalbd <alalbd@gmail.com>
2024-06-02 23:21:34 +0600

    Working with dashboard ui

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,5 @@
-<header class="header border-solid border-light-blue-500 border-1 border-y-0">
-    <div class="px-5 py-2">
+<header class="header ">
+    <div class="px-5 py-3 border-solid border-b border-gray-600">
         <div class="flex justify-between">
             <div class="logo">
                 <div class="text-whiteTrue-100 font-bold">Expense</div>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,1 +1,5 @@
-fff
+<div class="sidebar contents w-72 lg:flxed lg:flex lg:min-h-screen border-solid border-r border-gray-600">
+    <div class="text-white">
+        ddd
+    </div>
+</div>
