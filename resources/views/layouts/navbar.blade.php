358be7f3aa5383baf397d8d5a2d85309f3bd0a3d
alalbd <alalbd@gmail.com>
2024-06-02 19:23:51 +0600

    Working with ui

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,12 @@
+<header class="header border-solid border-light-blue-500 border-1 border-y-0">
+    <div class="px-5 py-2">
+        <div class="flex justify-between">
+            <div class="logo">
+                <div class="text-whiteTrue-100 font-bold">Expense</div>
+            </div>
+            <div class="action">
+                <div class="text-whiteTrue-100">Expense</div>
+            </div>
+        </div>
+    </div>
+</header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
+            colors: {
+                main: {
+                    100: "#18181b",
+                    200: "#333",
+                },
+                greenTrue: {
+                    100: "#34d399",
+                },
+                whiteTrue: {
+                    100: "#f1f1f2",
+                },
+            },
+
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
     },
